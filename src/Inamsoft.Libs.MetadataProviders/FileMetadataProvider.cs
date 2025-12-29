13e45fc7577ccae11a97882f685f438f9557aa73
muhammedinam <minam365@gmail.com>
2025-12-29 23:19:08 +0100

    Enhance documentation for FileMetadataProvider class with detailed summaries and remarks

--- src/Inamsoft.Libs.MetadataProviders/FileMetadataProvider.cs
+++ src/Inamsoft.Libs.MetadataProviders/FileMetadataProvider.cs
@@ -3,8 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace Inamsoft.Libs.MetadataProviders
 {
+    /// <summary>
+    /// Provides functionality to retrieve metadata for files using their file paths.
+    /// </summary>
+    /// <remarks>
+    /// This class extends <see cref="BaseMetadataProvider{FileMetadataProvider}"/> to leverage
+    /// the base functionality for metadata retrieval and implements <see cref="IMetadataProvider{TMetadata}"/>
+    /// to provide specific metadata processing for files using the <see cref="FileMetadata"/> container.
+    /// </remarks>
     public class FileMetadataProvider : BaseMetadataProvider<FileMetadataProvider>, IMetadataProvider<FileMetadata>
     {
+        /// <summary>
+        /// Represents a provider for retrieving metadata of files based on their paths or names.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="FileMetadataProvider"/> class provides functionality to retrieve file information
+        /// encapsulated within instances of the <see cref="FileMetadata"/> class. It relies on the
+        /// <see cref="BaseMetadataProvider{TMetadataProvider}"/> to access underlying file metadata operations
+        /// and implements <see cref="IMetadataProvider{TMetadata}"/> for metadata management specific to
+        /// file-based resources.
+        /// </remarks>
         public FileMetadataProvider(ILogger<FileMetadataProvider> logger) : base(logger)
         {
         }
